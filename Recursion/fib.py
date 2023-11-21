daff95b58242fbe543ce1f1dfbc7aa39d1a8833a
Noah Daniel <noahdaniel@utexas.edu>
2023-11-20 19:20:45 -0600

    fib and sum recursive functions

---
+++ Recursion/fib.py
@@ -0,0 +1,7 @@
+def fib(num):
+    if num == 1:
+        return 1
+    else:
+        return num * (num - 1)
+
+print(fib(3))
---
+++ Recursion/sum.py
@@ -0,0 +1,7 @@
+def sum(nums):
+    if len(nums) == 1:
+        return nums[0]
+    else:
+        return nums[0] + sum(nums[1:])
+
+print(sum([1,2,3,4,5]))
