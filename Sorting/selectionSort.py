1e6a7a4153c916142741609511c8925533a1a314
Noah Daniel <noahdaniel@utexas.edu>
2023-11-20 18:16:57 -0600

    added alternate solution to selectionSort

--- Sorting/selectionSort.py
+++ Sorting/selectionSort.py
@@ -10,3 +10,25 @@ def selectionSort(nums):
 
 arr = [-2, 45, 0, 11, -9,88,-97,-202,747]
 print(selectionSort(arr))
+
+
+#alternate solution
+
+def findSmallest(nums):
+    smallest = nums[0]
+    smallest_idx = 0
+    for i in range(len(nums)):
+        if nums[i] < smallest:
+            smallest_idx = i
+            smallest = nums[i]
+    return smallest_idx
+
+def altSelectionSort(nums):
+    sortedNums = []
+    for i in range(len(nums)):
+        smallest = findSmallest(nums)
+        sortedNums.append(nums.pop(smallest))
+    return sortedNums
+
+arr = [-2, 45, 0, 11, -9,88,-97,-202,747]
+print(altSelectionSort(arr))
