4c80d90cf66a9200245aa04bc96df2a0368836e9
Noah Daniel <noahdaniel@utexas.edu>
2023-11-20 19:48:29 -0600

    factorial with recurision

--- Recursion/fib.py
+++ Recursion/fib.py
@@ -1,7 +1,9 @@
-def fib(num):
-    if num == 1:
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1 or n == 2:
         return 1
     else:
-        return num * (num - 1)
+        return (fib(n-1) + fib(n-2))
 
-print(fib(3))
+print(fib(7))
