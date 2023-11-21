6af2bf57ac9dc0796016cef67d2a759bb842f514
Noah Daniel <noahdaniel@utexas.edu>
2023-11-20 20:46:29 -0600

    updated binarySearch recursive function

--- Recursion/binarySearch.py
+++ Recursion/binarySearch.py
@@ -1,18 +1,19 @@
 #binary search with recursion
-def binarySearch(nums, l, r, target):
-    if l <= r:
-        mid = (l + r) // 2
 
-        if nums[mid] == target:
-            return mid
-        elif nums[mid] > target:
-            return binarySearch(nums, l, mid - 1, target)
-        else:
-            return binarySearch(nums, mid + 1, r, target)
-        
-    else:
-        return -1
+def binarySearch(arr, target):
+    def recursive_helper(low, high):
+        if low <= high:
+            mid = (low + high) // 2
+
+            if arr[mid] == target:
+                return mid
+            elif arr[mid] > target:
+                return recursive_helper(low, mid - 1)
+            else:
+                return recursive_helper(mid + 1, high)
+    return recursive_helper(0, len(arr) - 1)
 
 arr = [ 2, 3, 4, 10, 40 ]
 x = 10
-print(binarySearch(arr, 0, len(arr)-1, x))
+
+print(binarySearch(arr, x))
