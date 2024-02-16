3b1ec9a68c1f62db27e39f9925ee587f8bcd1f7e
Noah Daniel <noahdaniel@utexas.edu>
2024-02-16 07:09:35 -0600

    isBadVersion

---
+++ Searching/isBadVersion.py
@@ -0,0 +1,16 @@
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        low = 1
+        hi = n
+
+        while low <= hi:
+            mid = (low + hi) // 2
+            if isBadVersion(mid):
+                hi = mid - 1
+            else:
+                left = mid + 1
+
+        return left
